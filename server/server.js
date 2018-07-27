dd7989804425751c728dcd6b777f4bb65f6652da
Rocksride <ihavenonameTaras@gmail.com>
2018-07-27 18:47:59 +0000

    created simple chat

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+
+app.get('/', function(request, response) {
+    response.sendFile("/index.html", {"root": "."});
+})
+
+io.on('connection', function(socket) {
+    const user = Date.now();
+
+    // socket.broadcast.emit('message', 'User '+user+' connected');
+    socket.on('message.sent', msg => {
+        
+        io.emit('message', `${user}: ${msg}`);
+    })
+    
+    io.emit('message', 'User '+user+' connected');
+    console.log('user connected', user)
+}) 
+
+http.listen(3000, function() {
+    console.log('server started');
+})
